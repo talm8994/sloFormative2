408f129cd4a6b2a9df6d010c4866a678649e28a2
talm8994 <tom.tom.pop13@gmail.com>
2016-03-23 12:40:32 -0600

    added really simple stuff fffff

---
+++ src/Controller/SloRunner.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public class SloRunner
+{
+	public static void main(String [] args)
+	{
+		SloController mySloController = new SloController();
+		SloController.start();
+		
+	}
+}
